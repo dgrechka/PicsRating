c9f0a7dc4175cd38c903f4a442c60e8d229d59f3
Dmitry A. Grechka <dmitry.grechka@gmail.com>
2016-04-16 14:19:29 +0300

    Adding CryptoSign. Cookie name and authword save

--- app.ts
+++ app.ts
@@ -1,5 +1,25 @@
  /// <reference path="typings/tsd.d.ts"/>
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+var CryptoJS: any;
+
 interface IPicture {
     GetURL():string;
     GetName():string;
@@ -39,25 +59,44 @@ var vm:any = {
     UserName: ko.observable(""),
     picA : ko.observable<IPicture>(pics[0]),
     picB : ko.observable<IPicture>(pics[1]),
+    Authword: ko.observable(""),
+    Authword2: ko.observable(""),
     CanRate: ko.pureComputed(() => {
-        return vm.UserName().length>0;
+        var nameFilled = vm.UserName().length>0;
+        var authworkFilled = vm.Authword().length>0;
+        var autwordsMatch = vm.AuthwordsMatch();
+        return nameFilled && authworkFilled && autwordsMatch;
+    }),
+    AuthwordsMatch:ko.pureComputed(() => {
+        return vm.Authword()===vm.Authword2();
     }),
     votedA: () => {
-        console.log("voted A");
+        console.log("voted left: prefer "+vm.picA().GetName()+" to "+vm.picB().GetName()+". UserSig "+ vm.NameSignature());
         Vote(vm.picA(),vm.picB());        
         RegenPics();
+        SaveCreds();
     },
     votedB: () => {        
-        console.log("voted B");
+        console.log("voted right: prefer "+vm.picB().GetName()+" to "+vm.picA().GetName()+". UserSig "+ vm.NameSignature());
         Vote(vm.picB(),vm.picA());
         RegenPics();
-    }
+        SaveCreds();
+    },
+    NameSignature: ko.pureComputed(() => {
+        return CryptoJS.MD5(vm.UserName()+vm.Authword());
+    }),
 };
+
+function SaveCreds() {
+    setCookie("username",vm.UserName(),9999);
+    setCookie("authword",vm.Authword(),9999);
+}
 
 function Vote(picWin: IPicture,picLose: IPicture) {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", votesRegistryPath+"?username="+vm.UserName()+"&picWin="+picWin.GetName()+"&picLose="+picLose.GetName(), true);
-    xhttp.send();
+    var voteHash =  CryptoJS.MD5(vm.UserName()+vm.Authword()+picWin.GetName()+picLose.GetName());
+    xhttp.open("GET", votesRegistryPath+"?voteID=1&username="+vm.UserName()+"&picWin="+picWin.GetName()+"&picLose="+picLose.GetName()+"&userSig="+vm.NameSignature()+"&voteSig="+voteHash, true);
+    xhttp.send();        
 }
 
 
@@ -86,7 +125,9 @@ function RegenPics() {
 
 window.onload = () => {
     RegenPics();
-    ko.applyBindings(vm);    
+    ko.applyBindings(vm);
+    vm.UserName(getCookie("username"));
+    vm.Authword(getCookie("authword"));    
 };
 
 //console.log("number of pics:"+pics.length);
